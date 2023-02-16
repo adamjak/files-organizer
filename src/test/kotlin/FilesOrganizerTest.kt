a0c5766169ebdc2f0b6c5a0cb653c0bf44400a29
Tomas Adamjak <tomas@adamjak.net>
2023-02-16 12:32:27 +0100

    add some tests

--- src/test/kotlin/FilesOrganizerTest.kt
+++ src/test/kotlin/FilesOrganizerTest.kt
@@ -6,6 +6,7 @@ import java.nio.file.attribute.FileTime
 import java.time.Instant
 import java.time.LocalDateTime
 import kotlin.test.assertFailsWith
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -16,14 +17,33 @@ class FilesOrganizerTest {
     private lateinit var tempDir2 : Path
 
     @BeforeEach
-    fun setup () {
+    fun beforeEach () {
         this.tempDir1 = Files.createTempDirectory("dir1")
         this.tempDir2 = Files.createTempDirectory("dir2")
     }
 
+    @AfterEach
+    fun afterEach () {
+
+        deleteDir(this.tempDir1.toFile())
+        deleteDir(this.tempDir2.toFile())
+    }
+
+    private fun deleteDir(file: File) {
+        val contents = file.listFiles()
+        if (contents != null) {
+            for (f in contents) {
+                if (!Files.isSymbolicLink(f.toPath())) {
+                    deleteDir(f)
+                }
+            }
+        }
+        file.delete()
+    }
+
 
     @Test
-    fun `Test organize folder` () {
+    fun `Test organize folder copy` () {
         val tempFile1 = File.createTempFile("temp", "", this.tempDir1.toFile())
         FilesOrganizer.Builder(
             inputFolderPath = this.tempDir1.toString(),
@@ -40,6 +60,50 @@ class FilesOrganizerTest {
         Assertions.assertTrue(createdDir.isDirectory)
         Assertions.assertEquals(strDate, createdDir.name)
         Assertions.assertTrue(createdDir.listFiles().any { f -> f.name.equals(tempFile1.name) })
+    }
+
+    @Test
+    fun `Test organize folder move` () {
+        File.createTempFile("temp", "", this.tempDir1.toFile())
+        FilesOrganizer.Builder(
+            inputFolderPath = this.tempDir1.toString(),
+            outputFolderPath = this.tempDir2.toString(),
+            move = true
+        ).build().organize()
+
+        Assertions.assertEquals(0,this.tempDir1.toFile().listFiles().size)
+        Assertions.assertEquals(1,this.tempDir2.toFile().listFiles().size)
+    }
+
+    @Test
+    fun `Test organize folder tree struct` () {
+        val tempFile1 = File.createTempFile("temp", "", this.tempDir1.toFile())
+        FilesOrganizer.Builder(
+            inputFolderPath = this.tempDir1.toString(),
+            outputFolderPath = this.tempDir2.toString(),
+            tree = true
+        ).build().organize()
+
+        Assertions.assertEquals(1,this.tempDir2.toFile().listFiles().size)
+
+        val now: LocalDateTime = LocalDateTime.now()
+
+        val yearDir = this.tempDir2.toFile().listFiles().first()
+        Assertions.assertTrue(yearDir != null)
+        Assertions.assertTrue(yearDir.isDirectory)
+        Assertions.assertEquals(now.year.toString(), yearDir.name)
+
+        val monthDir = yearDir.listFiles().first()
+        Assertions.assertTrue(monthDir != null)
+        Assertions.assertTrue(monthDir.isDirectory)
+        Assertions.assertEquals(String.format("%02d",now.month.value), monthDir.name)
+
+        val dayDir = monthDir.listFiles().first()
+        Assertions.assertTrue(dayDir != null)
+        Assertions.assertTrue(dayDir.isDirectory)
+        Assertions.assertEquals(String.format("%02d",now.dayOfMonth), dayDir.name)
+
+        Assertions.assertTrue(dayDir.listFiles().any { f -> f.name.equals(tempFile1.name) })
     }
 
     // region Builder
